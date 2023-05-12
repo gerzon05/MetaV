7cb4a578dc6a1adb231d9ecd43b1f71b556a8b77
gerzon05 <128724547+gerzon05@users.noreply.github.com>
2023-05-12 14:33:52 -0500

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -5,7 +5,7 @@ $(".menu").click(function(){
 $(".bus").click(function(){
     $(".buscador2").slideUp(1000);
 })
-$("video").hide();
+
 $(".imagen1").click(function (e) { 
     e.preventDefault();
     $("#video").slideDown(700);
@@ -13,3 +13,24 @@ $(".imagen1").click(function (e) {
 $("#video").dblclick(function(){
     $(this).slideUp(500);
 })
+$("a").click(function (e) {
+    e.preventDefault();
+})
+
+const openmodal = document.querySelector(".regis");
+const openmodal1 = document.querySelector("#regis");
+const modal = document.querySelector(".modal");
+const cerrar = document.querySelector(".cerrar1");
+
+openmodal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+openmodal1.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+cerrar.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
